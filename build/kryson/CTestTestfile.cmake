c8e31b06737098b95671850d256ccbd456ad7855
Antonio Rinaldi <a.rinaldi.dev@gmail.com>
2025-10-07 13:53:10 +0200

    Initial setup and first minimal publisher/subscriber

---
+++ build/kryson/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/antonio/ros2/src/kryson
+# Build directory: /home/antonio/ros2/build/kryson
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
