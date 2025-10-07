d3f38011d3c0077289fd9189cbbba889356af757
Antonio Rinaldi <a.rinaldi.dev@gmail.com>
2025-10-07 14:03:43 +0200

    Removing cached

--- build/kryson/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antonio/ros2/src/kryson
-# Build directory: /home/antonio/ros2/build/kryson
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
